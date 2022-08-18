64422078910cf4d69dfaebaa3b8bc9c60fda6730
MirianDuarte <contatomirianduarte@hotmail.com>
2022-08-18 18:20:33 -0300

    fundamentos web

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nome = window.document.getElementById ("nome")
+let email = window.document.getElementById ("email")
+let telefone = window.document.getElementById("telefone")
+let data_nascimento = window.document.getElementById ("data_nascimento")
+let cidade = window.document.getElementById ("cidade")
+let estado = window.document.getElementById ("estado")
+let endereco = window.document.getElementById ("endereco")
+let assunto = window.document.getElementById ("assunto")
+
+
